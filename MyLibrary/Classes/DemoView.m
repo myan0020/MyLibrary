7fdc2b8722b665b16737da3aa0c1735dec6541a4
yangming05 <843670891@qq.com>
2020-01-14 16:37:21 +0800

    DemoView created

---
+++ MyLibrary/Classes/DemoView.m
@@ -0,0 +1,20 @@
+//
+//  DemoView.m
+//  MyLibrary
+//
+//  Created by 杨鸣 on 2020/1/14.
+//
+
+#import "DemoView.h"
+
+@implementation DemoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
